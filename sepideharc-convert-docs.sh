a0d4d77f1be26a0c302ad80e2cbf223868f19e68
sepideh.arcangeli <sepideh.arcangeli@gmail.com>
2017-07-14 13:11:44 -0400

    added converted files

--- sepideharc-convert-docs.sh
+++ sepideharc-convert-docs.sh
@@ -2,12 +2,20 @@
 
 # Declare input argument as a variable
 
+a=myblogpost
+
 # Convert markdown to HTML
-
+pandoc -o $a.html $a.md
 # Convert markdown to DOCX
-
+pandoc -o $a.docx $a.md 
 # Convert markdown to ODT
-
+pandoc -o $a.odt $a.md
 # Convert markdown to PDF
+pandoc -S -o $a.pdf $a.md
 
-# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+#convert markdown to Latex
+pandoc $a --latex-engine=pdflatex -o $a.pdf 
+
+
+# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF"
+echo "Converted $a to HTML, DOCX, ODT, PDF , Latex"
